679aec9a0849ea2db8bfb825eb50fa25d2b1d89d
s23bzx9 <45764459+s23bzx9@users.noreply.github.com>
2019-01-15 14:48:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   echo "PHP Hello world. Generated at ".date("H:i:s d.m.Y")."<hr/>";
 
-  $conn_string="host=137.116.194.198 port=5432 dbname=postgres user=postgres password=";
+  $conn_string="host=137.116.194.198 port=5432 dbname=postgres user=postgres password=nnnn";
   $conn=pg_connect($conn_string);
   $result=pg_query($conn,"select * from public.msgs");
   $rows=pg_fetch_all($result)
