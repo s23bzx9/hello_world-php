3a01c5b5cc3e8e8975c474ce7b0262d2bffcb45c
s23bzx9 <45764459+s23bzx9@users.noreply.github.com>
2019-01-16 10:08:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-
   $conn_string="host=137.116.194.198 port=5432 dbname=postgres user=postgres";
   $conn=pg_connect($conn_string);
   $result=pg_query($conn,"select * from public.msgs");
   $rows=pg_fetch_array($result);
 
-
-  echo "PHP Hello world. Generated at ".date("H:i:s d.m.Y")."<hr/><br/><br/>";
-  echo "Print results from DB query";
+  echo "PHP Hello world. Generated at ".date("H:i:s d.m.Y")."<hr/><br/><br/>Print results from DB query<pre>";
   print_r($rows);
 ?>
