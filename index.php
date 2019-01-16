cccaa66e7ae633cc4fc2f145a21b5f5c2493c511
s23bzx9 <45764459+s23bzx9@users.noreply.github.com>
2019-01-16 09:37:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
   $conn_string="host=137.116.194.198 port=5432 dbname=postgres user=postgres password=nnnn";
   $conn=pg_connect($conn_string);
   $result=pg_query($conn,"select * from public.msgs");
-  $rows=pg_fetch_array($result)
+  $rows=pg_fetch_array($result);
 
-  print_r($rows);
+  echo "Print results from DB query";
+  // print_r($rows);
 ?>
