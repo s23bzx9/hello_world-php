b763823bf516597212712872543486a2988e3edb
s23bzx9 <45764459+s23bzx9@users.noreply.github.com>
2019-01-17 12:27:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
   $result=pg_query($conn,"select * from public.msgs");
   $rows=pg_fetch_all($result);
 
-  echo "PHP Hello world. Generated at ".date("H:i:s d.m.Y")." for IP: ".$_SERVER['REMOTE_ADDR']."<hr/><br/><br/>Print results from DB query<pre>";
+  echo "PHP Hello world 1. Generated at ".date("H:i:s d.m.Y")." for IP: ".$_SERVER['REMOTE_ADDR']."<hr/><br/><br/>Print results from DB query<pre>";
   print_r($rows);
 ?>
