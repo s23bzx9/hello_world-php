bd2ec7088e35591a6c5c6ac78605e861264103e8
s23bzx9 <45764459+s23bzx9@users.noreply.github.com>
2019-01-30 12:59:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-  $conn_string="host=137.116.194.198 port=5432 dbname=postgres user=postgres";
+  $db_host     = getenv('DATABASE_HOST');
+  $db_port     = getenv('DATABASE_PORT');
+  $db_name     = getenv('DATABASE_NAME');
+  $db_username = getenv('DATABASE_USER');
+  $db_password = getenv('DATABASE_PASSWORD');
+
+  $conn_string="host=$db_host port=$db_port dbname=$db_name user=$db_username".$db_password!=""?" password=$db_password":"";
   $conn=pg_connect($conn_string);
   $result=pg_query($conn,"select * from public.msgs");
   $rows=pg_fetch_all($result);
 
-  echo "PHP Hello world 1. Generated at ".date("H:i:s d.m.Y")." for IP: ".$_SERVER['REMOTE_ADDR']."<hr/><br/><br/>Print results from DB query<pre>";
+  echo "PHP Hello world. Generated at ".date("H:i:s d.m.Y")." for IP: ".$_SERVER['REMOTE_ADDR']."<hr/><br/><br/>Print results from DB query<pre>";
   print_r($rows);
 
   //phpinfo();
